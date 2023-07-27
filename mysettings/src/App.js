9063158952d9a09cb96fffa07a0d92b2f1f44259
Judiann <judiannpraise004@gmail.com>
2023-07-27 09:07:08 +0100

    fourth commit

--- mysettings/src/App.js
+++ mysettings/src/App.js
@@ -1,10 +1,20 @@
-
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <div className="container">
+        <h1>Job Setting</h1>
+
+        <div className="sub-container">
+          <h3>General Settings</h3>
+          <FontAwesomeIcon icon={faCalendar} /> 
+        </div>
+      </div>
     </div>
   );
 }
